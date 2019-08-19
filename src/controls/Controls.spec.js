74fdf02e3093008252806464925efd12eaab1c0f
Richard Lawson <lawson.a.richard@gmail.com>
2019-08-19 15:42:18 -0700

    Some tests
    some tests in controls ....if that is correct?

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { render } from "react-testing-library";
-
 import Controls from "./Controls";
+import Dashboard from "../dashboard/Dashboard";
 
-test("");
+describe("<Control/>", () => {
+  it("checks if close gate button is enabled", () => {
+    const { getByText } = render(<Controls closed={false} />);
+    getByText(/close gate/i);
+  });
+  it("checks if lock gate is button disabled as gate is OPEN", () => {
+    const { getByText } = render(<Controls locked={true} />);
+    getByText(/lock gate/i);
+  });
+});
